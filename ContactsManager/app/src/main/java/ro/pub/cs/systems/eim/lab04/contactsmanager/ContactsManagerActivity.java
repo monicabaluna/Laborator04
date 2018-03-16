a4dcd4ada2939ae2fed952f6ef6ecad8f6fc90a0
monicabaluna <monicamariabaluna@gmail.com>
2018-03-16 11:31:58 +0200

    Start project

---
+++ ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.lab04.contactsmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_manager);
+    }
+}
